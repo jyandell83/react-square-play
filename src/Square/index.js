9e3270060f268b01dccd34db7896682a81e0e43e
Jeremy Yandell <jereyand@gmail.com>
2019-09-29 15:35:34 -0700

    randomize square color and size

--- src/Square/index.js
+++ src/Square/index.js
@@ -9,8 +9,12 @@ const Square = () =>  {
     const [color, setColor] = useState('blue');
     const [size, setSize] = useState('300px')
     const handleClick = () => {
-        setColor('red');
-        setSize('200px')
+        let red = Math.floor(Math.random()*256);
+        let green = Math.floor(Math.random()*256);
+        let blue = Math.floor(Math.random()*256);
+        let size = Math.ceil(Math.random()*500);
+        setColor(`rgb(${red}, ${green}, ${blue})`);
+        setSize(`${size}px`)
     }
     return(
             <SquareShape color={color} size={size} onClick={() => handleClick()} />
